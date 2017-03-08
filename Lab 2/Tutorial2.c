846c31d27dedc7652652a1847958f2630d7e6711
raybeck95 <wheelman328@aim.com>
2017-03-08 17:33:49 -0500

    tutorial 2, red led on 2

--- Lab 2/Tutorial2.c
+++ Lab 2/Tutorial2.c
@@ -5,6 +5,8 @@ int main()
 {
 	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK; // Enable the clock to port B
 	PORTB->PCR[22] = PORT_PCR_MUX(001); // Set up PTB18 as GPIO
+	PTB->PDDR |= (1 << 22); 						// enable data direction out on 22
 	
-	
+	PTB->PCOR |= (1 << 22); 
+	PTB->PSOR |= (0 << 22);
 }
