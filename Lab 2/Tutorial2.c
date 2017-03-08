265b0f8909be8ba1364a9e1fa6a411da3630c07c
raybeck95 <wheelman328@aim.com>
2017-03-08 17:37:59 -0500

    tutorial 2, red and blue LEDs

--- Lab 2/Tutorial2.c
+++ Lab 2/Tutorial2.c
@@ -4,9 +4,17 @@
 int main()
 {
 	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK; // Enable the clock to port B
+	
 	PORTB->PCR[22] = PORT_PCR_MUX(001); // Set up PTB18 as GPIO
 	PTB->PDDR |= (1 << 22); 						// enable data direction out on 22
 	
+	PORTB->PCR[21] = PORT_PCR_MUX(001); // Set up PTB18 as GPIO
+	PTB->PDDR |= (1 << 21); 						// enable data direction out on 22
+	
+	// PCOR 1 clears bit, PSOR 1 sets bit
+	// Logic value of 0 = LED on, logic value 1 = LED off
 	PTB->PCOR |= (1 << 22); 
 	PTB->PSOR |= (0 << 22);
+	
+	PTB->PCOR |= (1 << 21);
 }
