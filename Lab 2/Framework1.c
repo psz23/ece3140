7893211040db94d3b7532c511345760a2dd6a128
Patricia Zhang <psz23@cornell.edu>
2017-03-08 17:40:52 -0500

    framework 1 init

--- Lab 2/Framework1.c
+++ Lab 2/Framework1.c
@@ -1,6 +1,7 @@
 #include <fsl_device_registers.h>
 
-/*int main (void)
+int main (void)
 {
-	    
-}*/
+	PIT->CHANNEL[0].LDVAL = 0x30000; // set load value of zeroth PIT
+	SIM->SCGC6 = SIM_SCGC6_PIT_MASK; // ENABLE CLOCK TO PIT MODULE
+}
