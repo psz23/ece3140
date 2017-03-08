2f338e25fcb2eee0cfcca0a9ddff436b8cdf0413
Patricia Zhang <psz23@cornell.edu>
2017-03-08 17:56:31 -0500

    framwork 1 stuff

--- Lab 2/Framework1.c
+++ Lab 2/Framework1.c
@@ -2,6 +2,10 @@
 
 int main (void)
 {
+	PIT->MCR = (1 << 0); // Enable PIT clocks
 	PIT->CHANNEL[0].LDVAL = 0x30000; // set load value of zeroth PIT
 	SIM->SCGC6 = SIM_SCGC6_PIT_MASK; // ENABLE CLOCK TO PIT MODULE
+	// PIT counts down and updates TFLG when it reaches 0
+	// poll TFLG
+	// CHANNEL[0].CVAL
 }
