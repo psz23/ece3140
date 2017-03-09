3beee58afd32c78ed5832f1b3b92adfc56c10418
raybeck95 <wheelman328@aim.com>
2017-03-08 19:09:24 -0500

    part 1 done!

--- Lab 2/Framework1.c
+++ Lab 2/Framework1.c
@@ -2,7 +2,7 @@
 
 const int TIMER_LENGTH = 0x01000000;
 
-void initLED(void); void LEDOff(void); void LEDOn(void); void LEDToggle(void);
+void initLED(void); void setLED(int);
 
 int main(void)
 {
@@ -10,17 +10,17 @@ int main(void)
 	
 	SIM->SCGC6 = SIM_SCGC6_PIT_MASK; // ENABLE CLOCK TO PIT MODULE
 	PIT->MCR &= ~(1 << 1); 					 // Enable PIT clocks (set 1st bit to 0)
-	PIT->CHANNEL[0].LDVAL = TIMER_LENGTH; // set load value of zeroth PIT
-	PIT->CHANNEL[0].TCTRL |= (1 << 0);	// Enable timer
+	PIT->CHANNEL[0].LDVAL = TIMER_LENGTH; // set load value of PIT 0
+	PIT->CHANNEL[0].TCTRL |= (1 << 0);	// Enable timer (begin counting)
 	PIT->CHANNEL[0].TFLG |= (1 << 0);		// Clear timer done flag
-			
+		
+	int isOn = 0;
+	
 	while(1)
 	{
-		int x = PIT->CHANNEL[0].CVAL;
-		int y = PIT->CHANNEL[0].TFLG;
-		if ((PIT->CHANNEL[0].TFLG & 1) == 1) 
-		{ 
-			LEDOn(); 
+		if ((PIT->CHANNEL[0].TFLG & 1) == 1) { 
+			setLED(~isOn); isOn = ~isOn;
+			PIT->CHANNEL[0].TFLG |= (1 << 0);
 		}
 	}
 }
@@ -30,23 +30,11 @@ void initLED(void)
 	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
 	PORTB->PCR[22] = PORT_PCR_MUX(001);
 	PTB->PDDR |= (1 << 22);
-	LEDOff();
+	setLED(0);
 }
 
-void LEDOff(void)
+void setLED(int set)
 {
-	PTB->PCOR &= ~(1 << 22);
-	PTB->PSOR |= (1 << 22);	
-}
-
-void LEDOn(void)
-{
-	PTB->PCOR |= (1 << 22);
-	PTB->PSOR &= ~(1 << 22);
-}
-
-void LEDToggle(void)
-{
-	PTB->PSOR ^= (1 << 22);
-	PTB->PCOR ^= (1 << 22);
+	if (set == 0) { PTB->PSOR |= (1 << 22);	}
+	else { PTB->PCOR |= (1 << 22); }
 }
